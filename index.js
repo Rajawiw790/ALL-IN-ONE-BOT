bcbd1f336b3c777fbee2f7a25bcc845b150ca25f
Rajawiw790 <108253254+Rajawiw790@users.noreply.github.com>
2026-07-09 19:18:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
     await loadEventHandlers(client);
     
     await delay(5000); 
-    require('./shiva');
- 
+    // require('./shiva'); // تم تعطيل التحقق
+console.log('[ SYSTEM ] Shiva Verification Bypassed ✅');
     console.log('\n[WAIT] Stabilizing before bot initialization...\n');
     await initializeBot();
     console.log('\n[READY] Bot fully initialized and running.\n');
